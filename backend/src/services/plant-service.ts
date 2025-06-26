5f7d6e1aee6bf37060607fb5dbedcdabd67bf343
Aneycan <nbandeira7@gmail.com>
2025-06-26 17:07:10 -0300

    primeiros componentes

--- backend/src/services/plant-service.ts
+++ backend/src/services/plant-service.ts
@@ -1,22 +1,22 @@
-import { PlantModel, IPlant } from '../models/plant-model'
+import { PlantModel, IPlant, IPlantDocument } from '../models/plant-model'
 export class PlantService {
-    static async criarPlanta(novaPlanta: IPlant): Promise<IPlant> {
+    static async criarPlanta(novaPlanta: IPlant): Promise<IPlantDocument> {
         return await PlantModel.create(novaPlanta);
     };
 
-    static async atualizarPlanta(nomePlanta: string, novosDados: Partial<IPlant>): Promise<IPlant> {
-        const plantaAtualizada = await PlantModel.findOneAndUpdate({nome: nomePlanta }, novosDados);
+    static async atualizarPlanta(nomePlanta: string, novosDados: Partial<IPlant>): Promise<IPlantDocument> {
+        const plantaAtualizada = await PlantModel.findOneAndUpdate({nome: nomePlanta }, novosDados, { new: true }); // para retornar o documento atualizado
         if(!plantaAtualizada) {
             throw new Error(`Nenhuma planta com o nome '${nomePlanta}' foi encontrada :/`);
         }
         return plantaAtualizada;
     };
 
-    static async mostrarPlanta(nomePlanta: string): Promise<IPlant[]> {
+    static async mostrarPlanta(nomePlanta: string): Promise<IPlantDocument[]> {
         return await PlantModel.find({ nome: nomePlanta });
     };
 
-    static async mostrarEstufa(): Promise<IPlant[]> {
+    static async mostrarEstufa(): Promise<IPlantDocument[]> {
         return await PlantModel.find();
     };
 }
